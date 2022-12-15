7b027111fecd3690a923fadcb1ef543f868c21f0
Viktor Demochko <91824125+AviaVik12@users.noreply.github.com>
2022-12-15 09:13:18 +0100

    delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// console.log('Hello from index.js');
-
-// const colors = require('colors');
-
-import colors from 'colors';
-
-const [u1, u2] = process.argv.splice(2);
-
-console.log(`Hello, ${colors.green(u1)} and ${colors.yellow(u2)}!`);
