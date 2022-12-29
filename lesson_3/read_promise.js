950151559948bddea75e6e8f01ca1a54d88ebdc4
Viktor Demochko <91824125+AviaVik12@users.noreply.github.com>
2022-12-29 10:24:03 +0100

    deleted unneeded directory

--- lesson_3/read_promise.js
+++
@@ -1,8 +0,0 @@
-// import fs from "fs";
-const fs = require("fs");
-const fsp = fs.promises;
-
-fsp.readFile("./access.log", { encoding: "utf-8" }).then(
-  (data) => console.log(data),
-  (error) => console.log(error)
-);
